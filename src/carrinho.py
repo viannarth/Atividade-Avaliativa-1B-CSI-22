d7b24c19ac018d76ae32c65a500ac67cbe136122
Arthur Vianna <viannarth@gmail.com>
2026-04-24 10:47:52 -0300

    change double leading underscore variables to single leading underscore

--- src/carrinho.py
+++ src/carrinho.py
@@ -3,34 +3,34 @@ from src.constantes import FormaPagamento
 
 class Carrinho():
     def __init__(self) -> None:
-        self.__carrinho:list[Bebida] = []
-        self.__valor_total:float = 0
-        self.__forma_pagamento:FormaPagamento = None
+        self._carrinho:list[Bebida] = []
+        self._valor_total:float = 0
+        self._forma_pagamento:FormaPagamento = None
 
     def consultarBebidas(self) -> list[Bebida]:
-        return self.__carrinho
+        return self._carrinho
     
     def consultarValorTotal(self) -> float:
-        return self.__valor_total
+        return self._valor_total
 
     def definirFormaPagamento(self, forma_pagamento:FormaPagamento) -> None:
-        self.__forma_pagamento = forma_pagamento
+        self._forma_pagamento = forma_pagamento
 
     def adicionarBebida(self, bebida:Bebida, num_bebidas:int = 1) -> None:
-        if bebida in self.__carrinho:
-            self.__carrinho[bebida] += num_bebidas
+        if bebida in self._carrinho:
+            self._carrinho[bebida] += num_bebidas
         else:
-            self.__carrinho.append(bebida)
-            self.__carrinho[bebida] = num_bebidas
-        self.__valor_total += bebida.consultarPreco()*num_bebidas
+            self._carrinho.append(bebida)
+            self._carrinho[bebida] = num_bebidas
+        self._valor_total += bebida.consultarPreco()*num_bebidas
 
     def removerBebida(self, bebida:Bebida, num_bebidas:int = 1) -> None:
-        self.__carrinho[bebida] -= num_bebidas
-        if self.__carrinho[bebida] == 0:
-            self.__carrinho.remove(bebida)
-        self.__valor_total -= bebida.consultarPreco()*num_bebidas
+        self._carrinho[bebida] -= num_bebidas
+        if self._carrinho[bebida] == 0:
+            self._carrinho.remove(bebida)
+        self._valor_total -= bebida.consultarPreco()*num_bebidas
 
     def esvaziarCarrinho(self) -> None:
-        for item in self.__carrinho:
-            self.__carrinho.remove(item)
-        self.__valor_total = 0
+        for item in self._carrinho:
+            self._carrinho.remove(item)
+        self._valor_total = 0
--- src/maquina.py
+++ src/maquina.py
@@ -1,30 +1,30 @@
 from src.constantes import TipoBebida
-from src.estoque import Item, Bebida, BebidaDosada, BebidaLata
+from src.estoque import Item, Bebida
 from src.carrinho import Carrinho
 
 class MaquinaVendas(): 
     def __init__(self) -> None:
-        self.__estoque:dict[Item, int] = {}
-        self.__vendas:dict[TipoBebida, float] = {}
+        self._estoque:dict[Item, int] = {}
+        self._vendas:dict[TipoBebida, float] = {}
 
     def consultarSaldo(self) -> float:
         saldo = 0
-        for tipo in self.__vendas:
-            saldo += self.__vendas[tipo]
+        for tipo in self._vendas:
+            saldo += self._vendas[tipo]
         return saldo
 
     def consultarEstoque(self) -> dict[Item, int]:
-        return self.__estoque
+        return self._estoque
 
     def consultarEstoqueItem(self, item:Item) -> int:
-        return self.__estoque[item]
+        return self._estoque[item]
 
     def consultarValorBebida(self, tipo_bebida:TipoBebida) -> float:
-        return self.__vendas[tipo_bebida]
+        return self._vendas[tipo_bebida]
 
     def atualizarVendas(self, bebida:Bebida, quantidade_vendida:int) -> None:
-        self.__vendas[bebida.consultarTipoBebida()] += bebida.consultarPreco()*quantidade_vendida
-        self.__estoque[bebida] -= quantidade_vendida
+        self._vendas[bebida.consultarTipoBebida()] += bebida.consultarPreco()*quantidade_vendida
+        self._estoque[bebida] -= quantidade_vendida
 
     def estocarItem(self, item:Item, quantidade_estoque:int) -> None:
         pass
